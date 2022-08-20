d7b5fe77a6633ac4ceed4e1f07a98411ee51a1fa
bgunn9in <52355738+bgunn9in@users.noreply.github.com>
2022-08-20 12:51:27 +0300

    Moar regexps

--- project/DuplicateFinder.py
+++ project/DuplicateFinder.py
@@ -10,8 +10,18 @@ class DuplicateFinder:
         pass
 
     def __regex_finder(self, data: str) -> str:
-        pattern = re.compile(r'\(.*\).*\[.*\]', re.IGNORECASE)
-        return pattern.findall(data)[0]
+        pattern = re.compile(r'\s+\(.*\).*\[.*\]', re.IGNORECASE)
+        result: list = pattern.findall(data)
+        if len(result) == 0:
+            pattern = re.compile(r'\s+\(.*\).*\[.*\)', re.IGNORECASE)
+            result: list = pattern.findall(data)
+            if len(result) == 0:
+                pattern = re.compile(r'\s+\(.*\)', re.IGNORECASE)
+                result: list = pattern.findall(data)
+                if len(result) == 0:
+                    b = 1
+
+        return result[0]
 
     def find(self, files: list) -> list:
         for file in files:
