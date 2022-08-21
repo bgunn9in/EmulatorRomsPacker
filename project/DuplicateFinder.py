8a57e181d5b79ffef6ce561c47ff92ff9acf587b
bgunn9in <52355738+bgunn9in@users.noreply.github.com>
2022-08-22 01:58:50 +0300

    Duplicates find wip

--- project/DuplicateFinder.py
+++ project/DuplicateFinder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import logging
 # coding: utf-8
 
 import re
@@ -7,25 +7,42 @@ import re
 
 class DuplicateFinder:
     def __int__(self):
+        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='main.log', encoding='utf-8', level=logging.DEBUG)
         pass
 
-    def __regex_finder(self, data: str) -> str:
-        pattern = re.compile(r'\s+\(.*\).*\[.*\]', re.IGNORECASE)
-        result: list = pattern.findall(data)
-        if len(result) == 0:
-            pattern = re.compile(r'\s+\(.*\).*\[.*\)', re.IGNORECASE)
-            result: list = pattern.findall(data)
-            if len(result) == 0:
-                pattern = re.compile(r'\s+\(.*\)', re.IGNORECASE)
-                result: list = pattern.findall(data)
-                if len(result) == 0:
-                    b = 1
+    @staticmethod
+    def __filter(region_data: str) -> bool:
+        regions: list = ['(U)', '(E)', '(UE)', '(W)', ]
+        for region in regions:
+            if region in region_data:
+                return False
+        return True
 
-        return result[0]
+    def __regex_finder(self, file: str, files: list) -> list:
+        regex_list: list = [
+            re.compile(r'\s+\(.*\).*\[.*\]', re.IGNORECASE),
+            re.compile(r'\s+\(.*\).*\[.*\)', re.IGNORECASE),
+            re.compile(r'\s+\(.*\)', re.IGNORECASE),
+        ]
+        clean_data: list = []
+        for regex in regex_list:
+            result: list = regex.findall(file)
+            if len(result) == 0:
+                logging.warning(f'Can\'t get regex, result: {file}')
+                continue
+            name: list = file.split(result[0])
+            if len(name) < 2:
+                logging.warning(f'Can\'t get game name, result: {name}')
+                continue
+            if self.__filter(result[0]):
+                logging.info(f'Skip file {file} by filter')
+                continue
+            clean_data.append(f'{name[0]} {result[0]} {name[1]}')
+        return clean_data
 
     def find(self, files: list) -> list:
         for file in files:
-            self.__regex_finder(file)
+            self.__regex_finder(file, files)
         return []
 
 
